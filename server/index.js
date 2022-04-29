e23d74560a5705beaa04c9178c12519f39ae33ec
OkuhleM <lindokuhlea38@gmail.com>
2022-04-29 15:46:03 +0200

    final changes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const mongoose = require('./model/mongoose');
-// const { UserDetails } = require('../capture-user-details/src/Components/UserDetails');
+const dotenv = require('dotenv')
+dotenv.config()
 
 var app = express();
 const port = 4001
-
+mongoose.connect("mongodb+srv://okuhle22:Lindokuhle22@cluster0.neoue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
 app.use(express.json())
 
-app.post ('/get-names',(req,res)=>{
+app.post ('/get-names',async (req,res)=>{
+    try{
     const {name , surname, dateOfBirth,grade} = req.body
     if(name == "" || surname == "" || dateOfBirth == "" || grade == ""){
         return res.send(400)
     }
     const UserDetails =  new mongoose({name ,surname, dateOfBirth, grade }) 
-            const value =   UserDetails.save()   
-            console.log(value, UserDetails)      
-            res.send(value)
-    })
+            console.log(UserDetails)      
+            res.send(UserDetails)
+}catch(err){
+    console.log(err)
+    res.send(err)
+}
+})
 
-
-    app.get('/get-names',(req,res)=>{
+    app.get('/get-names_id',(req,res)=>{
         const userDetails = mongoose.find()
         res.send(userDetails)
         console.log(userDetails)
